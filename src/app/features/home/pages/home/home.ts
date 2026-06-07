f90062408c8e89662afcdc5d8d57500ca81186a6
ajaymalah <ajaymalah.2003@gmail.com>
2026-06-07 22:54:23 +0530

    sdf

--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { PublicLayout } from '../../../../layouts/public-layout/public-layout';
-
 import { Hero } from '../../components/hero/hero';
-import { About } from '../../components/about/about';
 import { Experience } from '../../components/experience/experience';
+import { About } from '../../components/about/about';
 import { Project } from '../../components/project/project';
 import { Skills } from '../../components/skills/skills';
 
+
 @Component({
   selector: 'app-home',
-  imports: [PublicLayout, Hero, About, Experience, Project, Project,Skills],
+  imports: [PublicLayout, Hero, About, Experience, Project,Skills],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
