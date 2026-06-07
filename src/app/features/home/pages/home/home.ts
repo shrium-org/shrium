8b4821624101b61373c4e28892c5e7fc3e7d02b1
ajaymalah <ajaymalah.2003@gmail.com>
2026-06-07 19:45:52 +0530

    sdf

--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { PublicLayout } from '../../../../layouts/public-layout/public-layout';
-import { Hero } from '../../components/hero/hero';
-import { About } from '../../components/about/about';
-import { Experience } from "../../components/experience/experience";
-import { Projects } from '../../components/projects/projects';
+
 import { Products } from "../../../../shared/components/products/products";
-import { Skills } from '../../components/skills/skills';
-import { Contact } from '../../components/contact/contact';
-import { ImpactComponent } from '../../components/impact.component/impact.component';
+import { Hero } from '../../../../shared/components/hero/hero';
+import { About } from '../../../../shared/components/about/about';
+import { Projects } from '../../../../shared/components/projects/projects';
 
 @Component({
   selector: 'app-home',
-  imports: [PublicLayout, Hero, About, Experience, Projects, Skills,Contact,ImpactComponent],
+  imports: [PublicLayout, Hero, About, Projects],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
