1a327af289798ac0dd7e1cdbb2e130e582d0e073
ajaymalah <ajaymalah.2003@gmail.com>
2026-06-08 17:32:31 +0530

    sdf

--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
-import { EducationComponent } from '../../components/education/education.component';
-import { ExperienceComponent } from '../../components/experience/experience.component';
-import { ProjectsComponent } from '../../components/projects/projects.component';
-import { SkillsComponent } from '../../components/skills/skills.component';
-import { FooterComponent } from '../../components/footer/footer.component';
-import { MetricsBannerComponent } from '../../components/metrics-banner/metrics-banner.component';
-import { PublicLayout } from '../../../../layouts/public-layout/public-layout';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PublicLayout,HeroComponent],
+  imports: [HeroComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
