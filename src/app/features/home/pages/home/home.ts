f690647b25dfed2127d5d1b33fffd7df204a1209
ajaymalah <ajaymalah.2003@gmail.com>
2026-06-08 15:36:45 +0530

    sdf

--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { EducationComponent } from '../../components/education/education.component';
 import { ExperienceComponent } from '../../components/experience/experience.component';
-import { MetricsBannerComponent } from '../../components/metrics-banner/metrics-banner.component';
-import { SkillsComponent } from '../../components/skills/skills.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
+import { SkillsComponent } from '../../components/skills/skills.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-
-
+import { MetricsBannerComponent } from '../../components/metrics-banner/metrics-banner.component';
 
 @Component({
   selector: 'app-home',
