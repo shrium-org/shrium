da126a27ae443e18dba4995d24738108c987e326
ajaymalah <ajaymalah.2003@gmail.com>
2026-07-28 19:21:10 +0530

    feat: certification api fixed

--- src/app/features/portfolio/pages/portfolio/portfolio.ts
+++ src/app/features/portfolio/pages/portfolio/portfolio.ts
@@ -16,4 +16,74 @@ import { CertificationComponent } from '../../home-components/certification/cert
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.css',
 })
-export class Portfolio {}
+export class Portfolio {
+
+  animatedSkills = [
+
+  {
+    name: 'Java',
+    icon: 'ti ti-coffee',
+    x:'10%',
+    y:'20%',
+    delay:'0s'
+  },
+
+  {
+    name:'Spring Boot',
+    icon:'ti ti-leaf',
+    x:'80%',
+    y:'15%',
+    delay:'2s'
+  },
+
+  {
+    name:'Angular',
+    icon:'ti ti-brand-angular',
+    x:'15%',
+    y:'70%',
+    delay:'4s'
+  },
+
+  {
+    name:'Kafka',
+    icon:'ti ti-brand-kafka',
+    x:'75%',
+    y:'60%',
+    delay:'1s'
+  },
+
+  {
+    name:'Docker',
+    icon:'ti ti-brand-docker',
+    x:'50%',
+    y:'10%',
+    delay:'3s'
+  },
+
+  {
+    name:'Kubernetes',
+    icon:'ti ti-cloud',
+    x:'85%',
+    y:'80%',
+    delay:'5s'
+  },
+
+  {
+    name:'Redis',
+    icon:'ti ti-database',
+    x:'30%',
+    y:'40%',
+    delay:'2.5s'
+  },
+
+  {
+    name:'CQRS',
+    icon:'ti ti-code',
+    x:'65%',
+    y:'35%',
+    delay:'1.5s'
+  }
+
+];
+
+}
